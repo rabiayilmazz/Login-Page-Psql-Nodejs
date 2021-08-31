472323876f001058b2968f365ce0771bef5a6135
Rabia Yılmaz <rabiarabiabbb@gmail.com>
2021-08-31 14:13:20 +0300

    dashboard

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.set("view engine", "ejs");
+
 app.get('/', (req,res)=>{
-    res.send("Hello");
+    res.render('index');
 });
 
+app.get('users/login', (req, res)=>{
+    res.render("login")
+});
+
+app.get('users/register', (req, res)=>{
+    res.render("register")
+});
+
+app.get('users/dashboard', (req, res)=>{
+    res.render("dashboard", {user: "Rabia"})
+});
 
 app.listen(PORT, () =>{
     console.log(`server server çalışıyor port: ${PORT}`);
-})
+});
 
 
