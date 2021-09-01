cbd19024a0dc4770e727d8e1e9c31db6d16757e4
Rabia Yılmaz <rabiarabiabbb@gmail.com>
2021-09-01 21:05:05 +0300

    jhgf

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', (req,res)=>{
     res.render('index');
 });
 
-app.get('users/login', (req, res)=>{
-    res.render("login")
+app.get('/users/login', (req, res)=>{
+    res.render('login')
 });
 
-app.get('users/register', (req, res)=>{
-    res.render("register")
+app.get('/users/register', (req, res)=>{
+    res.render('register')
 });
 
-app.get('users/dashboard', (req, res)=>{
-    res.render("dashboard", {user: "Rabia"})
+app.get('/users/dashboard', (req, res)=>{
+    res.render('dashboard', {user: 'Rabia'});
 });
 
 app.listen(PORT, () =>{
