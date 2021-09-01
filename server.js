5434a5f9d435451c267e61b1ccf6faf89a57b7aa
Rabia Yılmaz <rabiarabiabbb@gmail.com>
2021-09-02 00:32:19 +0300

    db connect

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const {pool} = require("./dbConfig");
 
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req,res)=>{
     res.render('index');
