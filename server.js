44cd07e803445a3b8f8b9589e93557c390f39ece
Rabia Yılmaz <rabiarabiabbb@gmail.com>
2021-09-02 12:38:01 +0300

    sdfg

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const {pool} = require("./dbConfig");
+const bcrypt = require('bcrypt');
 
 const PORT = process.env.PORT || 8080;
 
@@ -23,7 +24,7 @@ app.get('/users/dashboard', (req, res)=>{
     res.render('dashboard', {user: 'Rabia'});
 });
 
-app.post('/users/register', (req, res)=>{
+app.post('/users/register', async (req, res)=>{
     let {name, email, password, password2} = req.body;
 
     console.log({
@@ -38,7 +39,7 @@ app.post('/users/register', (req, res)=>{
     if(!name || !email || !password || !password2){
         errors.push({message: "Bütün alanları eksiksiz doldurun!"});
     }
-    
+
     if(password.length < 6){
         errors.push({message: "Parola 6 karakterden büyük olmalı"});
     }
@@ -49,6 +50,20 @@ app.post('/users/register', (req, res)=>{
 
     if(errors.length>0){
         res.render('register', {errors});
+    }else{
+        let hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+
+        pool.query(
+            `Select * from users
+            where email= $1,` [email], (err, result)=>{
+                if(err){
+                    throw err;
+                }
+
+                console.log(result.rows);
+            }
+        )
     }
 })
 app.listen(PORT, () =>{
